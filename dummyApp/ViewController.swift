5b208c8f731aa9bf1def739725f71dc7b50ef878
Anubhav <programmingwithnaubhav@gmail.com>
2024-12-04 15:58:30 +0530

    updates by Anubhav

--- dummyApp/ViewController.swift
+++ dummyApp/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var myName: UITextField!
+    
 
+    @IBOutlet weak var myGithub: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        
         // Do any additional setup after loading the view.
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        segue.destination.title = myName.text
+        
+        let github = myGithub.text
+        let name = myName.text
+        
+        if let destinationVC = segue.destination as? SecondViewController {
+            destinationVC.name = name
+            destinationVC.github = github
+        }
+        
+        
+    }
+    
+    
 
 }
 
